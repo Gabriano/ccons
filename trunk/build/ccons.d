5b2801fbe4acdd4243d109a4d5a2af71f3b6f622
Eric Thivierge <thiveri@gmail.com>
2016-06-11 22:10:09 -0400

    First commit

---
+++ trunk/build/ccons.d
@@ -0,0 +1,3 @@
+build/ccons.o: src/ccons.c include/ccons.h
+src/ccons.c:
+include/ccons.h:
---
+++ trunk/src/ccons.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <ccons.h>
+
+int main(int argc, const char *argv[]) {
+    printf("Hello World!");
+}
